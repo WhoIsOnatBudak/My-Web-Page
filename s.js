0bdebdc1da1815a81d396a39f140e892afda2a51
onat <onatbudak@gmail.com>
2024-07-27 22:34:49 +0300

    Did it worth it?

--- s.js
+++ s.js
@@ -1,4 +1,4 @@
-let cur =1;
+/*let cur =1;
 
 showSlide(cur);
 
@@ -21,4 +21,40 @@ function showSlide(n){
     dots[n-1].style.backgroundColor = "black";
     slides[n-1].style.display = "block";
     cur = n;
+}*/
+//const apiUrl = 'http://api.travelpayouts.com/v1/prices/cheap?origin=IST&destination=AMS&depart_date=2024-09&return_date=2024-09&token=86808928e22299e8fd0e979cd1cb8294&currency=TRY&market=TR';
+
+
+/*async function fetchFlightPrices() {
+    
+        
+        const response = await fetch('http://api.travelpayouts.com/v1/prices/cheap?origin=IST&destination=AMS&depart_date=2024-09&return_date=2024-09&token=86808928e22299e8fd0e979cd1cb8294&currency=TRY&market=TR');
+        
+
+        
+
+        const veri = await response.json();
+        
+        
+        console.log(veri);
+
+}*/
+
+const theUrl = "http://api.travelpayouts.com/v1/prices/cheap?origin=IST&destination=AMS&depart_date=2024-09&return_date=2024-09&token=86808928e22299e8fd0e979cd1cb8294&currency=TRY&market=TR";
+
+/*async function fetchFlightPrices(theUrl)
+{
+
+    const response = await fetch(theUrl, {
+		method: 'GET',
+		headers: {
+			'Connection': 'keep-alive',
+			'Accept-Encoding': 'gzip, deflate, br',
+            //'Accept': ''
+		},
+	})
 }
+*/
+fetch(theUrl)
+  .then((response) => response.json())
+  .then((json) => console.log(json));
