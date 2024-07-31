ebfb5c941a8284b81f4b5320051a35c505d5e202
onat <onatbudak@gmail.com>
2024-07-31 12:46:24 +0300

    So, lets get RICH boys!

--- s.js
+++ s.js
@@ -18,17 +18,17 @@ function showSlide(n){
     for (let i = 0; i < dots.length; i++) {
         dots[i].style.backgroundColor = "#f5f5f5";
     }
-    dots[n-1].style.backgroundColor = "#a9a9a9";
+    dots[n-1].style.backgroundColor = "#684545";
     slides[n-1].style.display = "block";
     cur = n;
 }
-//const apiUrl = 'http://api.travelpayouts.com/v1/prices/cheap?origin=IST&destination=AMS&depart_date=2024-09&return_date=2024-09&token=86808928e22299e8fd0e979cd1cb8294&currency=TRY&market=TR';
+//const apiUrl = 'http://api.travelpayouts.com/v1/prices/cheap?origin=IST&destination=AMS&depart_date=2024-09&return_date=2024-09&token=&currency=TRY&market=TR';
 
 
 /*async function fetchFlightPrices() {
     
         
-        const response = await fetch('http://api.travelpayouts.com/v1/prices/cheap?origin=IST&destination=AMS&depart_date=2024-09&return_date=2024-09&token=86808928e22299e8fd0e979cd1cb8294&currency=TRY&market=TR');
+        const response = await fetch('http://api.travelpayouts.com/v1/prices/cheap?origin=IST&destination=AMS&depart_date=2024-09&return_date=2024-09&token=&currency=TRY&market=TR');
         
 
         
@@ -45,7 +45,8 @@ function showSlide(n){
     redirect: "follow"
   };
   
-  fetch('http://api.travelpayouts.com/v1/prices/cheap?origin=IST&destination=AMS&depart_date=2024-09&return_date=2024-09&token=86808928e22299e8fd0e979cd1cb8294&currency=TRY&market=TR', requestOptions)
+  fetch('http://api.travelpayouts.com/v1/prices/cheap?origin=IST&destination=AMS&depart_date=2024-09&return_date=2024-09&token=
+  &currency=TRY&market=TR', requestOptions)
     .then((response) => response.text())
     .then((result) => document.getElementById("pri").innerHTML = result)
     .catch((error) => console.error(error));*/
